5f3e581ba9e1943eadc8f777858357416e037ea4
aboumaza <ad.boumaza@yahoo.fr>
2020-04-16 13:01:57 +0200

    t

--- src/test/java/dz/algerie/app/AlgerieDataApplicationTests.java
+++ src/test/java/dz/algerie/app/AlgerieDataApplicationTests.java
@@ -3,10 +3,10 @@ package dz.algerie.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class AlgerieDataApplicationTests {
 
-	@Test
+	// s@Test
 	void contextLoads() {
 	}
 
